167c00e0e15783d416c58602a4fd24003a511b6f
junior-rosa83 <148802542+junior-rosa83@users.noreply.github.com>
2024-01-24 09:06:20 -0300

    Add files via upload

---
+++ scripts/focusTimer/sounds.js
@@ -0,0 +1,5 @@
+export const bgAudio = new Audio ('./assets/bg-audio.mp3')
+export const record = new Audio ('./assets/record.mp3')
+export const pew = new Audio ('./assets/pew.mp3')
+
+bgAudio.loop = true
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+import './toggle-mode.js'
+import * as focusTimer from './focusTimer/index.js'
+
+focusTimer.start(0, 3)
